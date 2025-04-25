9fcadfe4cc2e66152cdcbe878215760e591b0cca
jecsct <jetravassos2001@gmail.com>
2025-04-25 12:06:51 +0100

    Initial commit

---
+++ order-service/src/test/java/com/personal_projects/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_projects.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ shipping-service/src/test/java/com/personal_projects/shipping_service/ShippingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_projects.shipping_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
