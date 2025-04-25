c8d14cd08025ec67d0e3aa5c5adbc076cf34b2c3
jecsct <jetravassos2001@gmail.com>
2025-04-25 19:02:20 +0100

    Initial setup
    Every service runs in it's respective port. Database connections still not established

--- shipping-service/src/main/java/com/personal_projects/shipping_service/ShippingServiceApplication.java
+++ shipping-service/src/main/java/com/personal_projects/shipping_service/ShippingServiceApplication.java
@@ -8,6 +8,7 @@ public class ShippingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShippingServiceApplication.class, args);
+		System.out.println("Shipping Service Application Started");
 	}
 
 }
